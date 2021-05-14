1e8e66a8dc8b985a947a3688a61de4babcff675a
Naren Chandrasekaran <Naren.Chandrasekaran@mu-sigma.com>
2021-05-14 12:10:58 +0530

    Shipping view

--- src/app/shipping/shipping.component.ts
+++ src/app/shipping/shipping.component.ts
@@ -12,7 +12,16 @@ export class ShippingComponent implements OnInit {
   @Output() updateShippingStage = new EventEmitter<object>();
 
   selectedProductSize: any = '';
-  selectedProductIndex: any = 0;
+  selectedProductIndex: any = -1;
+
+  shippingDetails = {
+    apartment: "",
+    landmark: "",
+    location: "",
+    nearestCentre: "",
+    pincode: "",
+    region: ""
+  }
 
 
   constructor() { }
@@ -24,10 +33,42 @@ export class ShippingComponent implements OnInit {
     if(this.selectedProductSize == selectedProductSize && this.selectedProductIndex == selectedProductIndex){
       this.selectedProductIndex = 0;
       this.selectedProductSize = '';
+
+      this.cart.items[selectedProductSize][selectedProductIndex].shippingDetails = Object.assign({},this.shippingDetails);
     }else{
       this.selectedProductSize = selectedProductSize;
       this.selectedProductIndex = selectedProductIndex;
+
+      this.shippingDetails = Object.assign({},this.cart.items[selectedProductSize][selectedProductIndex].shippingDetails);
     }
+  }
+
+  saveButtonAction(productSize: any, index: any){
+    console.log("shipping details: ",this.shippingDetails);
+    this.cart.items[productSize][index].shippingDetails = Object.assign({},this.shippingDetails);
+
+    this.selectedProductSize = '';
+      this.selectedProductIndex = -1;
+
+    this.shippingDetails = {
+      apartment: "",
+      landmark: "",
+      location: "",
+      nearestCentre: "",
+      pincode: "",
+      region: ""
+    }
+    
+    this.handleBoxConfigUpdate(this.cart);
+  }
+
+  discardButtonAction(productSize: any, index: any){
+    this.shippingDetails = Object.assign({},this.cart.items[productSize][index].shippingDetails);
+  }
+
+  handleBoxConfigUpdate(event: any) {
+    console.log(event);
+    this.updateShippingStage.emit(event);
   }
 
 }
