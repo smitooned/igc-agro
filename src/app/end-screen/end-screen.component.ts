0a253261548c08e6bc1881cd3b5acbc35c70f8de
docssptorShM18#o@ <smit.shah@mu-sigma.com>
2021-05-15 09:33:56 +0530

    updated whatsapp message

--- src/app/end-screen/end-screen.component.ts
+++ src/app/end-screen/end-screen.component.ts
@@ -11,30 +11,95 @@ export class EndScreenComponent implements OnInit {
   constructor() { }
 
   @Input() cart: any;
+  @Input() priceList: any;
+
+  cartCost: any = {
+    A1:0,
+    A2:0,
+    A3:0,
+    total:0
+  }
   orderText: any;
 
   ngOnInit(): void {
   }
 
   buildOrder() {
+    let dummy = String.fromCharCode(10);
     this.orderText = `
-    Order ID: *${this.cart.orderID}*
+Order ID:
+*${this.cart.orderID}*
     `;
-    
-    this.cart.items.A1.map((item:any) => {
-      let dummy = String.fromCharCode(10);
-      this.orderText += `
-        *${item.rxtx.to}'s* ${item.productType}${dummy}
-        *Personal Note:* ${item.rxtx.message}${dummy}
-        *From:* ${item.rxtx.from}${dummy}
-        *Size:* ${item.productSize}${dummy}
-        *Box Arrangment:* ${item.boxArrangement}${dummy}
-        *Address:* ${item.shippingDetails.apartment}, ${item.shippingDetails.location}${dummy}
-        *Pincode:* ${item.shippingDetails.pincode}${dummy}
-        *Nearest Landmark:* ${item.shippingDetails.landmark}${dummy}
-        *Cost:* ${item.productCost}
-    `
+
+    // let cartCost = {
+    //   A1:0,
+    //   A2:0,
+    //   A3:0,
+    //   total:0
+    // };
+
+    Object.keys(this.cart.items).map((size:any) => {
+      if(this.cart.items[size].length > 0) {
+        let unitPrice = this.priceList.COVIDBox[size];
+        this.cartCost[size] = unitPrice * this.cart.items[size].length;
+        this.cartCost['total'] += this.cartCost[size]
+        this.orderText +=`
+--------------
+COVID WARRIOR KIT
+Size: ${size}
+Qty: ${this.cart.items.A1.length}
+Unit Price: Rs ${unitPrice}/-
+Total: *Rs ${this.cartCost[size]}*${dummy}
+Box(s):-
+        `
+        
+        this.cart.items[size].map((item:any, index:any) => {
+          this.orderText += `
+${index+1})*To:* ${item.rxtx.to}*
+    *Personal Note:* ${item.rxtx.message}
+    *From:* ${item.rxtx.from}
+    *Box Arrangment:* ${item.boxArrangement}
+    *Address:* ${item.shippingDetails.apartment}, ${item.shippingDetails.location}
+    *Pincode:* ${item.shippingDetails.pincode}
+    *Nearest Landmark:* ${item.shippingDetails.landmark}${dummy}
+        `
+        })
+      }
     })
+
+    this.orderText += `
+
+______________
+
+Grand Total: Rs *${this.cartCost.total}*/-
+______________
+    `;
+
+    // this.cart.items.A1.map((item:any) => {
+    //   let dummy = String.fromCharCode(10);
+    //   this.orderText += `
+    //   A1 Size Alphonso ${item.productType}
+    //   *To:* ${item.rxtx.to}'s* ${item.productType}
+    //   *Personal Note:* ${item.rxtx.message}
+    //   *From:* ${item.rxtx.from}
+    //   *Size:* ${item.productSize}
+    //   *Box Arrangment:* ${item.boxArrangement}
+    //   *Address:* ${item.shippingDetails.apartment}, ${item.shippingDetails.location}
+    //   *Pincode:* ${item.shippingDetails.pincode}
+    //   *Nearest Landmark:* ${item.shippingDetails.landmark}
+    //   *Cost:* ${item.productCost}
+    // `
+    // })
+
+    // this.orderText +=`
+    // --------------
+    // COVID WARRIOR KIT
+    // Size: A1
+    // Qty: ${this.cart.items.A1.length}
+    // Unit Price: Rs 499/-
+    // Total: *Rs ${499*this.cart.items.A1.length}*
+    // --------------
+    // `
     console.log(this.orderText);
 
     let res = encodeURI(this.orderText);
