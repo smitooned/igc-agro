336467217cae82fee2be04e674a1bdc72eb8720d
docssptorShM18#o@ <smit.shah@mu-sigma.com>
2021-05-20 00:27:05 +0530

    updates to provision different pack size

--- src/app/checkout/checkout.component.ts
+++ src/app/checkout/checkout.component.ts
@@ -15,9 +15,11 @@ export class CheckoutComponent implements OnInit {
   priceA2: any = 599;
   priceA3: any = 699;
 
-  totalA1Cost: any = 0;
-  totalA2Cost: any = 0;
-  totalA3Cost: any = 0;
+  totalA1_10_Cost: any = 0;
+  totalA2_6_Cost: any = 0;
+  totalA2_10_Cost: any = 0;
+  totalA3_6_Cost: any = 0;
+  totalA3_10_Cost: any = 0;
   amountDonated: any = 0;
   totalCost: any = 0;
 
@@ -29,11 +31,17 @@ export class CheckoutComponent implements OnInit {
 
   ngOnInit(): void {
 
-    this.totalA1Cost = this.priceA1 * this.cart.items["A1"].length;
-    this.totalA2Cost = this.priceA2 * this.cart.items["A2"].length;
-    this.totalA3Cost = this.priceA3 * this.cart.items["A3"].length;
-    this.amountDonated = 99 * (this.cart.items["A1"].length + this.cart.items["A2"].length + this.cart.items["A3"].length)
-    this.totalCost = this.totalA1Cost + this.totalA2Cost + this.totalA3Cost;
+    this.totalA1_10_Cost = this.priceList['COVIDWarriorKit']['A1_10'] * this.cart.items["A1"].length;
+
+    this.totalA2_6_Cost = (this.priceList['COVIDWarriorKit']['A2_6'] * this.getCount('6', "A2"));
+    this.totalA2_10_Cost = (this.priceList['COVIDWarriorKit']['A2_10'] * this.getCount('10', "A2"));
+
+    this.totalA3_6_Cost = (this.priceList['COVIDWarriorKit']['A3_6'] * this.getCount('6', "A3"));
+    this.totalA3_10_Cost = (this.priceList['COVIDWarriorKit']['A3_10'] * this.getCount('10', "A3"));
+
+    this.amountDonated = 99 * (this.cart.items["A1"].length + this.cart.items["A2"].length + this.cart.items["A3"].length);
+
+    this.totalCost = this.totalA1_10_Cost + this.totalA2_6_Cost + this.totalA2_10_Cost + this.totalA3_6_Cost + this.totalA3_10_Cost;
   }
 
   showDetails(selectedProductSize: any, selectedProductIndex: any) {
@@ -43,6 +51,15 @@ export class CheckoutComponent implements OnInit {
     } else {
       this.selectedProductSize = selectedProductSize;
       this.selectedProductIndex = selectedProductIndex;
+    }
+  }
+
+  getCount(packOf:any, productSize:any) {
+    if(this.cart.items[productSize].length > 0) {
+      let count = this.cart.items[productSize].filter((x:any) => x["packOf"] == packOf).length;
+      return count
+    } else {
+      return 0
     }
   }
 
--- src/app/end-screen/end-screen.component.ts
+++ src/app/end-screen/end-screen.component.ts
@@ -19,12 +19,41 @@ export class EndScreenComponent implements OnInit {
     A3:0,
     total:0
   }
+
+  totalA1_10_Cost: any = 0;
+  totalA2_6_Cost: any = 0;
+  totalA2_10_Cost: any = 0;
+  totalA3_6_Cost: any = 0;
+  totalA3_10_Cost: any = 0;
+  amountDonated: any = 0;
+  totalCost: any = 0;
+
   orderText: any;
 
   ngOnInit(): void {
   }
 
   buildOrder() {
+
+    this.totalA1_10_Cost = this.priceList['COVIDWarriorKit']['A1_10'] * this.cart.items["A1"].length;
+
+    this.totalA2_6_Cost = (this.priceList['COVIDWarriorKit']['A2_6'] * this.getCount('6', "A2"));
+    this.totalA2_10_Cost = (this.priceList['COVIDWarriorKit']['A2_10'] * this.getCount('10', "A2"));
+
+    this.totalA3_6_Cost = (this.priceList['COVIDWarriorKit']['A3_6'] * this.getCount('6', "A3"));
+    this.totalA3_10_Cost = (this.priceList['COVIDWarriorKit']['A3_10'] * this.getCount('10', "A3"));
+
+    this.amountDonated = 99 * (this.cart.items["A1"].length + this.cart.items["A2"].length + this.cart.items["A3"].length);
+
+    this.totalCost = this.totalA1_10_Cost + this.totalA2_6_Cost + this.totalA2_10_Cost + this.totalA3_6_Cost + this.totalA3_10_Cost;
+
+    this.cartCost = {
+      A1: this.totalA1_10_Cost,
+      A2: this.totalA2_6_Cost + this.totalA2_10_Cost,
+      A3: this.totalA3_6_Cost + this.totalA3_10_Cost,
+      total: this.totalCost
+    }
+
     let dummy = String.fromCharCode(10);
     this.orderText = `
 Order ID:
@@ -40,15 +69,15 @@ Order ID:
 
     Object.keys(this.cart.items).map((size:any) => {
       if(this.cart.items[size].length > 0) {
-        let unitPrice = this.priceList.COVIDBox[size];
-        this.cartCost[size] = unitPrice * this.cart.items[size].length;
-        this.cartCost['total'] += this.cartCost[size]
+        // let unitPrice = this.priceList.COVIDWarriorKit[size + "_" + ];
+        // this.cartCost[size] = unitPrice * this.cart.items[size].length;
+        // this.cartCost['total'] += this.cartCost[size]
         this.orderText +=`
 --------------
 COVID WARRIOR KIT
 Size: ${size}
-Qty: ${this.cart.items[size].length}
-Unit Price: Rs ${unitPrice}/-
+Qty: ${this.getQuantity(size)}
+Unit Price: ${this.getUnitPrice(size)}
 Total: *Rs ${this.cartCost[size]}*${dummy}
 Box(s):-
         `
@@ -59,6 +88,7 @@ ${index+1})*To:* ${item.rxtx.to}
     *Personal Note:* ${item.rxtx.message}
     *From:* ${item.rxtx.from}
     *Box Arrangment:* ${item.boxArrangement}
+    *Box Selected:* Pack of ${item.packOf}
     *Address:* ${item.shippingDetails.address_line_1}, ${item.shippingDetails.address_line_2}, ${item.shippingDetails.city}
     *Pincode:* ${item.shippingDetails.pincode}
     *Nearest Landmark:* ${item.shippingDetails.landmark}
@@ -111,6 +141,41 @@ ______________
     //send on whatsapp
     var win = window.open(`https://wa.me/${+918050027648}?text=${res}`, '_blank');
 
+  }
+
+  getQuantity(productSize:any) {
+    if(productSize == "A1") {
+      return `
+        Pack of 10: ${this.getCount("10","A1")}
+      `
+    } else {
+      return `
+      Pack of 6: ${this.getCount("6", productSize)}
+      Pack of 10: ${this.getCount("10", productSize)}
+      `
+    }
+  }
+
+  getUnitPrice(productSize:any) {
+    if(productSize == "A1") {
+      return `
+        Pack of 10: ${this.priceList["COVIDWarriorKit"]["A1_10"]}
+      `
+    } else {
+      return `
+      Pack of 6: ${this.priceList["COVIDWarriorKit"][productSize+"_6"]}
+      Pack of 10: ${this.priceList["COVIDWarriorKit"][productSize+"_10"]}
+      `
+    }
+  }
+
+  getCount(packOf:any, productSize:any) {
+    if(this.cart.items[productSize].length > 0) {
+      let count = this.cart.items[productSize].filter((x:any) => x["packOf"] == packOf).length;
+      return count
+    } else {
+      return 0
+    }
   }
 
 }
